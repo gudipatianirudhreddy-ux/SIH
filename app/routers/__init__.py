bf5356adc212aff146d93be55eaad977a74f9a4d
gudipatianirudhreddy-ux <gudipati.anirudh.reddy@gmail.com>
2026-09-22 17:45:26 +0530

    feat: register points router

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,6 +2,7 @@ from app.routers.application import router as application_router
 from app.routers.dashboard import router as dashboard_router
 from app.routers.evidence import router as evidence_router
 from app.routers.issue import router as issue_router
+from app.routers.points import router as points_router
 from app.routers.profile import router as profile_router
 from app.routers.solution import router as solution_router
 from app.routers.sponsorship import router as sponsorship_router
@@ -9,6 +10,7 @@ from app.routers.sponsorship import router as sponsorship_router
 __all__ = [
     "profile_router",
     "issue_router",
+    "points_router",
     "solution_router",
     "application_router",
     "evidence_router",
