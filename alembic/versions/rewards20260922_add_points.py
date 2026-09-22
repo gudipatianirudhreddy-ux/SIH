d99ec6e5f4f6e60748c64ae037f3255deaaed9ec
gudipatianirudhreddy-ux <gudipati.anirudh.reddy@gmail.com>
2026-09-22 17:45:57 +0530

    fix: chain points migration after latest schema

--- alembic/versions/rewards20260922_add_points.py
+++ alembic/versions/rewards20260922_add_points.py
@@ -1,13 +1,13 @@
 """add points and point transaction ledger
 Revision ID: rewards20260922
-Revises: 6177d08b0a33
+Revises: f58c93b12a41
 """
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 revision: str = "rewards20260922"
-down_revision: Union[str, Sequence[str], None] = "6177d08b0a33"
+down_revision: Union[str, Sequence[str], None] = "f58c93b12a41"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
